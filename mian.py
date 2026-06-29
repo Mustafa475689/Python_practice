46698a1708e92dbb10da0e808527b9f0aedb5de9
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-29 14:35:11 +0500

    practice python
    practice python

---
+++ mian.py
@@ -0,0 +1,38 @@
+print("AOA")
+
+# type 
+name = "ajjo"
+print(type(name))
+
+v = 32j  # complex data type
+
+# unicode
+unicode =   "😂"
+print(ord(unicode))
+
+# Slicing 
+myName = "Mustafa Khan"
+print(myName[0:7:1])
+print(myName[8:12:1])
+
+# Type conversion
+a = 22
+a = str(22)
+
+# formated string
+print("hello my name is ",myName)
+print(f"My name is {myName}")
+
+# Input 
+age = input("Write your age here ")
+print(age)
+
+# type conversion input
+agee = int(input("what is your age"))
+print(agee)
+
+# Data Types #
+# List 
+# tupple
+# dictionary
+# sets
