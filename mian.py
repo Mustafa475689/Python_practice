a21f26ecf782898294d9c193a183b827c99ca283
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-29 17:15:52 +0500

    Refactor and restore content in mian.py
    Refactor and restore content in mian.py

--- mian.py
+++ mian.py
@@ -5,6 +5,11 @@ name = "ajjo"
 print(type(name))
 
 v = 32j  # complex data type
+# Data Types #
+# List 
+# tupple
+# dictionary
+# sets
 
 # unicode
 unicode =   "😂"
@@ -24,15 +29,81 @@ print("hello my name is ",myName)
 print(f"My name is {myName}")
 
 # Input 
-age = input("Write your age here ")
-print(age)
+# age = input("Write your age here ")
+# print(age)
 
 # type conversion input
-agee = int(input("what is your age"))
-print(agee)
+# agee = int(input("what is your age"))
+# print(agee)
 
-# Data Types #
-# List 
-# tupple
-# dictionary
-# sets
+# Arithmetic Operators
+a = 7
+b = 3
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# flow divisio. Flow division converts the float values into integer it is symbolized by (//)
+# print(a//b)
+# exponensial operator (**)
+# print(5**2)
+# print(32 % 5) # modulus operation its means it will provide the remainder
+
+# Aassignment operators
+# Compound assignment operators
+# (a = a + 3) == (a += 3) 
+# +=, *=, -=, /=, //=, **=
+
+# Comparasion operators
+# <, >, ==, !=, <=, >=
+# print(a != 7)
+# print(ord("Mustafa"[0]))
+# print(ord("Khan"[0]))
+# print("Mustafa" < "Khan")
+
+# Logical Operators
+# and, or, not
+# not reverse the boolean values 
+# print(12 == 12)
+# print(not 12 == 12)
+
+# Conditions 
+# if a < b:
+#     print("True")
+# else:
+#     print("False")
+
+# money = int(input("How much you have $"))
+# ganey = int(input("How much you have $"))
+
+# if money > ganey:
+#     print(f"{money} is greater")
+
+# elif ganey > money:
+#     print(f"{ganey} is greater")
+# else:
+#     print("Both are eqaul")
+
+# if money == 10:
+#     print("You can get the choco bar")
+# elif money == 20:
+#     print("You can get the Monkey bar")
+# else:
+#     print("You can get the Corneto")
+
+# name = str(input("What is your name "))
+# age = int(input("what is your age"))
+
+# if age >= 18:
+#     print(f"{name} you can vote")
+# else: 
+#     print(f"{name} you are not old enough to vote")
+
+# Loops #
+# a = range(1,21,1)
+# for i in a:
+#     print(i)
+
+# for i in range(21):
+#     print(i)
